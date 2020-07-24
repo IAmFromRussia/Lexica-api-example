c2ff4b26e2e9ef8b4017808bb6ffa6feeef272ca
Danila <permogorsky.danila@gmail.com>
2020-07-24 13:36:37 +0300

    Init

---
+++ LexicaApiExample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LexicaApiExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
